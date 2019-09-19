d8767b8dc0282d6b2c1002aa75470f648d2d0333
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-19 14:26:56 -0300

    Atualizando Rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 
-function Films() {
-    return <h2>Films</h2>;
-}
-
-function Characters() {
-    return <h2>Characters</h2>;
-}
-
-function Starships() {
-    return <h2>Starships</h2>;
-}
-
-function Vehicles() {
-    return <h2>Vehicles</h2>;
-}
-
-function Planets() {
-    return <h2>Planets</h2>;
-}
-
-function Species() {
-    return <h2>Species</h2>;
-}
-
-function PageNotFound() {
-    return <h2>PageNotFound</h2>;
-}
-
 export default function Routes() {
     return (
         <BrowserRouter>
