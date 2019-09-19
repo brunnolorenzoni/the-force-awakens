dd6ccd9c723eae8ec90d96e55184a07e21a6cba9
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-19 13:05:04 -0300

    Atualizando rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,13 +33,13 @@ export default function Routes() {
     return (
         <BrowserRouter>
         <Switch>
-            <Route exact  path="/" exact component={Films} />
-            <Route exact path="/films/:id" component={Films} />
-            <Route exact path="/characters/:id" component={Characters} />
-            <Route exact path="/starships/:id" component={Starships} />
-            <Route exact path="/vehicles/:id" component={Vehicles} />
-            <Route exact path="/planets/:id" component={Planets} />
-            <Route exact path="/species/:id" component={Species} />
+        <Route exact path="/" component={Home} />
+            <Route path="/films/:id?" component={Films} />
+            <Route path="/characters/:id?" component={Characters} />
+            <Route path="/starships/:id?" component={Starships} />
+            <Route path="/vehicles/:id?" component={Vehicles} />
+            <Route path="/planets/:id?" component={Planets} />
+            <Route path="/species/:id?" component={Species} />
             <Route exact path='*' component={PageNotFound}/>
         </Switch>
     </BrowserRouter>
