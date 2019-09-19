254ac024fdf8bbc2e57139825ee2973c984e3e45
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2019-09-19 14:32:08 -0300

    Criando header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Header from "../../components/Header/Header"
+
 export default function Home() {
 
     return (
-        <h1>Home!</h1>
+        <>
+            <Header />
+            <h1>Home!</h1>
+        </>
     )
 }
